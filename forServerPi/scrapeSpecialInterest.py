95ccaf277d45c23d37aaa93c01865ee54d2b32c6
HelenRice <h.rice2@newcastle.ac.uk>
2016-06-08 16:27:59 +0100

    Update scrapeSpecialInterest.py
    Updated special interest topic.

--- forServerPi/scrapeSpecialInterest.py
+++ forServerPi/scrapeSpecialInterest.py
@@ -4,7 +4,7 @@ import twythonSpecialInterestTool
 import txt2wavTool
 import subprocess
 
-#run the script that scrapes the latest IVFresearch tweets, parses them and then saves them into an XML file
+#run the script that scrapes the latest breastfeeding tweets, parses them and then saves them into an XML file
 twythonSpecialInterestTool
 
 #run the script that takes our tweets in XML format and converts them to an audio file
