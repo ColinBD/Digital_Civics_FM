1bd8712588bf2dfe45a7b7c0145d70ecaa6f13cf
ColinBD <colin8ll@yahoo.co.uk>
2016-06-09 15:13:30 +0100

    Add files via upload
    added the version with Stuart's comments

--- forRadioPi/browser.sh
+++ forRadioPi/browser.sh
@@ -1,10 +1,18 @@
 #/!bin/bash
 
+#Check to see if the browser is open 
 if ! ps -e | grep epiphany > /dev/null
+
+#If the browser is open...
 then
+
+	# export xauthority
     export XAUTHORITY="/home/pi/.Xauthority"
+	#export display
     export DISPLAY=:0.0
-    echo "DISPLAY is $DISPLAY"
+    #echo "DISPLAY is $DISPLAY"
+	
+	#run epiphany web browser to localhost file, sleep 5, simulate mouse movement off-screen and then sleep for one hour. Simulate F5 keypress to re-load index.html 
     epiphany-browser -a --profile /home/pi/.config http://127.0.0.1/index.html & sleep 5 && xdotool mousemove_relative --sync 512 600 & sleep 3600 && xdotool key F5
 fi
 
